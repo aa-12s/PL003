b6fa38f2ae362291da17fe7181835e6277192a0d
aa-12s <aa_suvorov@mail.ru>
2024-09-17 21:25:39 +0400

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,34 @@
+# Способы вызова функции. Задача "Рассылка писем"
+
+
+
+
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    if ('@' or '.com' or '.ru' or '.net') not in recipient:
+        if ('@' or '.com' or '.ru' or '.net') not in sender:
+    # if("@"and(".com"or".ru"or".net")) not in (recipient or sender) or("@" or(".com"or"ru"or".net")) not in (recipient or sender):
+
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+# Если же sender и recipient совпадают, то вывести "Нельзя отправить письмо самому себе!"
+
+# if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or "ru" or ".net")) not in (
+#         recipient or sender):
+#     # создаём  1 условие
+#     print(f"Невозможно отправить письмо с адреса {sender}на адрес {recipient}")
+# elif recipient == sender:
+#     # создём 2 условие
+#     print("Нельзя отправить письмо самому себе!")
+#     # создаём 3 условие
+# elif sender == "university.help@gmail.com":
+#     print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}.")
+#     # создаём 4 условие
+# elif sender != "university.help@gmail.com":
+#     print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+#     # вывод результата
+#     send_email('messagje', 'university.help@mail.com', '@.com')
+#     send_email('messagje', 'university.help@mail.com')
+#     send_email('messagje', 'universi@.com')
+#     send_email('messagje', 'universi@.co')
+#     send_email('messagje', 'universi@.com')
