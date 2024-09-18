10739dbb7db52a27b65540caa1e346c7cc193df9
aa-12s <aa_suvorov@mail.ru>
2024-09-18 18:22:32 +0400

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,34 +1,21 @@
-# Способы вызова функции. Задача "Рассылка писем"
+# Способы вызова функции& Задача "Рассылка писем"
 
 
-
-
-def send_email(message, recipient, sender = "university.help@gmail.com"):
-    if ('@' or '.com' or '.ru' or '.net') not in recipient:
-        if ('@' or '.com' or '.ru' or '.net') not in sender:
-    # if("@"and(".com"or".ru"or".net")) not in (recipient or sender) or("@" or(".com"or"ru"or".net")) not in (recipient or sender):
-
-            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+def send_email(message, recipient, sender='university.help@gmail.com'):
+    list_ = ['.com', '.ru', '.net']
+    if (not any(i in recipient for i in list_) or not any(i in sender for i in list_)
+            or '@' not in recipient or '@' not in sender):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        return
     elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
-# Если же sender и recipient совпадают, то вывести "Нельзя отправить письмо самому себе!"
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
-# if ("@" and (".com" or ".ru" or ".net")) not in (recipient or sender) or ("@" or (".com" or "ru" or ".net")) not in (
-#         recipient or sender):
-#     # создаём  1 условие
-#     print(f"Невозможно отправить письмо с адреса {sender}на адрес {recipient}")
-# elif recipient == sender:
-#     # создём 2 условие
-#     print("Нельзя отправить письмо самому себе!")
-#     # создаём 3 условие
-# elif sender == "university.help@gmail.com":
-#     print(f"Письмо успешно отправлено с адреса{sender} на адрес {recipient}.")
-#     # создаём 4 условие
-# elif sender != "university.help@gmail.com":
-#     print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-#     # вывод результата
-#     send_email('messagje', 'university.help@mail.com', '@.com')
-#     send_email('messagje', 'university.help@mail.com')
-#     send_email('messagje', 'universi@.com')
-#     send_email('messagje', 'universi@.co')
-#     send_email('messagje', 'universi@.com')
+
+send_email('message', 'vasyok1337@gmail.com')
+send_email('message', 'urban.fan@mail.ru')
+send_email('message', 'urban.student@mail.ru')
+send_email('message', 'university.help@gmail.com')
