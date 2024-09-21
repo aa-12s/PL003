870f18f9ef9fe2157de17250365dcdba4b3345ee
aa-12s <aa_suvorov@mail.ru>
2024-09-21 13:40:22 +0400

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,21 +1,33 @@
+
 # Способы вызова функции& Задача "Рассылка писем"
 
 
 def send_email(message, recipient, sender='university.help@gmail.com'):
-    list_ = ['.com', '.ru', '.net']
-    if (not any(i in recipient for i in list_) or not any(i in sender for i in list_)
-            or '@' not in recipient or '@' not in sender):
+    if '@' not in recipient or '@' not in sender:
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-        return
-    elif recipient == sender:
-        print('Нельзя отправить письмо самому себе!')
-    elif sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
     else:
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+        list_ = ('.com', '.ru', '.net')
+        if not recipient.endswith(list_) or not sender.endswith(list_):
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        elif recipient == sender:
+            print('Нельзя отправить письмо самому себе!')
+        elif sender == 'university.help@gmail.com':
+            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+        else:
+            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
 
 
 send_email('message', 'vasyok1337@gmail.com')
 send_email('message', 'urban.fan@mail.ru')
 send_email('message', 'urban.student@mail.ru')
 send_email('message', 'university.help@gmail.com')
+
+
+
+
+
+
+
+
+
+
