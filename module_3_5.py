d84f995de4ee1fd5f089c1fd57f57c38f06b6e2f
aa-12s <aa_suvorov@mail.ru>
2024-09-22 15:15:15 +0400

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -3,12 +3,13 @@
 
 def get_multiplied_digits(number):
     str_number = str(number)
+    str_number = str_number.replace('0', '')
+    first = int(str_number[0])
     if len(str_number) > 1:
-        first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return int(str_number)
 
 
-result = get_multiplied_digits(40203)
+result = get_multiplied_digits(4020300)
 print(result)
